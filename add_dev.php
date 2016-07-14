9b06343dfa2c16c061b0cc28fa54701e864f7bc4
Alex Nye <alnye655321@gmail.com>
2016-07-14 15:52:28 -0600

    changed Hours to double value in SQL, now verifying hours is a number in add.dev. Added Source column in activity table SQL. Now recording user that adds a new dev

--- add_dev.php
+++ add_dev.php
@@ -20,7 +20,7 @@ if(!empty($nameSearch)){
 	$splitFirst = explode(", ", $nameSearch);
 	$LastName = $splitFirst[0];
 	$FirstName = $splitFirst[1];
-	
+
 	$getID = mysqli_fetch_assoc(mysqli_query($con,"SELECT DISTINCT SNum FROM TeachingInfo WHERE LastName = '$LastName' AND FirstName = '$FirstName'"));
 	$SNum = $getID["SNum"];
 }
@@ -29,17 +29,17 @@ if(!empty($nameSearch)){
 function getChairEmail($SNum) {
 	global $con;
 	global $con2;
-	
+
 	$getID = mysqli_fetch_assoc(mysqli_query($con,"SELECT DISTINCT Subject FROM TeachingInfo WHERE SNum = '$SNum' LIMIT 1"));
-	$Prefix = $getID["Subject"];	
+	$Prefix = $getID["Subject"];
 
 	$email = "onlinelearning@ccaurora.edu";
-	
+
 	$result = mysqli_query($con2,"SELECT Depts, Email FROM Users WHERE Chair = '1'");
-	
+
 	while($row = mysqli_fetch_array($result))	{
 		$prefixList = $row['Depts']; $emailGrab = $row['Email'];
-		
+
 		$prefixSplit=explode(" ",$prefixList);
 	   foreach($prefixSplit as $value) {
 	   	if($value == $Prefix) {
@@ -60,7 +60,7 @@ function getChairEmail($SNum) {
 <head>
 	<meta charset="utf-8">
 	<title>Add Prof Dev</title>
-	
+
 
 
 	<!-- jQuery -->
@@ -90,7 +90,7 @@ function getChairEmail($SNum) {
 
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 	<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-	
+
 	<script>
 	$(function() {
 	    $( "#nameSearch" ).autocomplete({
@@ -99,7 +99,7 @@ function getChairEmail($SNum) {
 	    });
 	});
 	</script>
-	
+
 <!-- /jQuery UI autocomplete -->
 
 
@@ -107,14 +107,14 @@ function getChairEmail($SNum) {
 <script type="text/javascript">
             // When the document is ready
             $(document).ready(function () {
-                
+
                 $('#Date').datepicker({
-                    format: "mm/dd/yyyy", 
+                    format: "mm/dd/yyyy",
                     startDate: "06/21/2016"
-                });  
-            
-            });          
-         
+                });
+
+            });
+
 </script>
 <!-- Close Initialize Datepicker-->
 
@@ -180,7 +180,7 @@ function getChairEmail($SNum) {
             </div>
         </div>
      </form></div>
-     
+
  <div class="col-md-3"><form method="post" action="add_dev.php">
      <div class="input-group">
          <input type="text" class="form-control ui-widget" placeholder="Search SNum" name="searchSNum" id="searchSNum">
@@ -198,14 +198,14 @@ function getChairEmail($SNum) {
 
 // SQL Add
 if($formSubmit == "true"){
-	
 
-	
+
+
 		$uploadOk = 1; $target_file = NULL;
 		if($_FILES["fileToUpload"]["error"] == 0) {
 			include 'upload.php';
 			}
-	
+
 	$SNum = $_POST['SNum'];
 	$LastName = $_POST['LastName'];
 	$FirstName = $_POST['FirstName'];
@@ -217,60 +217,61 @@ if($formSubmit == "true"){
 	$Sponsor = $_POST['Sponsor'];
 	$Hours = $_POST['Hours'];
 	$Comments = $_POST['Comments'];
-	
+
 	if(!empty($Other)){$Type = $_POST['Other'];} // check if other is empty for type
-	
+
 	$Item=mysqli_real_escape_string($con, $Item);
 	$Item = str_replace(",",";",$Item);
 	$Date=mysqli_real_escape_string($con, $Date);
 	$Date = str_replace(",",";",$Date);
 	$Sponsor=mysqli_real_escape_string($con, $Sponsor);
 	$Sponsor = str_replace(",",";",$Sponsor);
-	$Hours=mysqli_real_escape_string($con, $Hours);
-	$Hours = str_replace(",",";",$Hours);
 	$Comments=mysqli_real_escape_string($con, $Comments);
 	$Comments = str_replace(",",";",$Comments);
-	
+	if (!is_numeric($Hours) || $Hours > 100) { // if hours is not numeric or greater than 100 insert blank value - error
+		$Hours = NULL;
+	}
+
 	$getID = mysqli_fetch_assoc(mysqli_query($con,"SELECT DISTINCT Subject FROM TeachingInfo WHERE SNum = '$SNum' LIMIT 1"));
 	$Prefix = $getID["Subject"];
-	
+
 	$result = mysqli_query($con,"SELECT id FROM Activity ORDER BY id DESC LIMIT 1");
 	$row = mysqli_fetch_array($result);
 	$id=$row['id'] + 1;
-	
-	$sql2 = "INSERT INTO Activity (id, SNum, LastName, FirstName, Type, Item, Date, Sponsor, Hours, Prefix, Comments, File)
-	VALUES ('$id','$SNum','$LastName','$FirstName','$Type','$Item','$Date','$Sponsor','$Hours','$Prefix','$Comments','$target_file')";
-	
+
+	$sql2 = "INSERT INTO Activity (id, SNum, LastName, FirstName, Type, Item, Date, Sponsor, Hours, Prefix, Comments, File, Source)
+	VALUES ('$id','$SNum','$LastName','$FirstName','$Type','$Item','$Date','$Sponsor','$Hours','$Prefix','$Comments','$target_file','$user')";
+
 	if (mysqli_query($con, $sql2)){
-	
+
 		echo "New record created successfully";}
 		else {
 	    echo "Error: " . $sql2 . "<br>" . mysqli_error($con);
 	}
-	
+
 	if($uploadOk = 0) {
 		echo '<br>File Upload Error: '.$uploadError.'<br>';
 	}
-	
+
 	//Missing Level entry check
 	$result = mysqli_query($con,"SELECT DISTINCT SNum FROM Level WHERE SNum = '$SNum'");
-	$row_cnt = mysqli_num_rows($result);	
-	
+	$row_cnt = mysqli_num_rows($result);
+
 	if($row_cnt != 1) {
 		$Level = 1; //create Level entry, starting at 1
 		$sql2 = "INSERT INTO Level (SNum, FirstName, LastName, Prefix, Level)
-		VALUES ('$SNum','$FirstName','$LastName','$Prefix','$Level')";	
+		VALUES ('$SNum','$FirstName','$LastName','$Prefix','$Level')";
 		mysqli_query($con, $sql2);
 	}
 	//close Missing Level entry check
-	
+
 	//send email to chair
 	$message = "New Prof Dev Activity Posted for Approval \r\n
 	Name : $LastName  ,  $FirstName  \r\n
 	Type: $Type  \r\n
 	Item: $Item";
 	email(getChairEmail($SNum),"alnye655321@gmail.com", "CCA ProDev - Activity Addition", $message);
-	
+
 
 }
 
@@ -301,7 +302,7 @@ echo "if(this.value=='Other')
 {document.getElementById('lab1').style.display = 'inherit';}
  else {
  	document.getElementById('lab1').style.display = 'none';};";
-echo '">"'; 
+echo '">"';
 echo '<option>Activity</option>';
 echo '<option>D2L</option>';
 echo '<option>MPT</option>';
@@ -345,7 +346,7 @@ echo '</div>';
  <div style="position:relative;"> <!--file upload styling/script-->
  <a class='btn btn-primary' href='javascript:;'>
  Choose File...
-<input type="file" name="fileToUpload" id="fileToUpload" style='position:absolute;z-index:2;top:0;left:0;filter: alpha(opacity=0);-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";opacity:0;background-color:transparent;color:transparent;' 
+<input type="file" name="fileToUpload" id="fileToUpload" style='position:absolute;z-index:2;top:0;left:0;filter: alpha(opacity=0);-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";opacity:0;background-color:transparent;color:transparent;'
 size="40"  onchange='$("#upload-file-info").html($(this).val());'>
 </a>
 &nbsp;
@@ -368,5 +369,3 @@ echo '</form>';
 
 </body>
 </html>
-
-
